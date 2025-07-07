776b95e843eb23ade993b22026a6c160e361531a
RishikaReddyMusku <rishikamusku20gmail.com>
2025-07-07 18:23:29 +0530

    firstcommit

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,20 @@
-// backend/db.js
-const mysql = require('mysql2/promise');
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.stack);
+    return;
+  }
+  console.log('✅ Connected to Railway MySQL DB!');
+});
+
+module.exports = connection;
